75ae3022a209450b88a76a706bc7f2d4c3fdfd92
Yousef Khaled <117599930+YousefKhaled53@users.noreply.github.com>
2023-05-06 00:42:50 +0300

    Add project files.

---
+++ Gym Project/Pages/homepage_3.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gym_Project.Pages
+{
+    public class homepage_3Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Gym Project/Pages/profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gym_Project.Pages
+{
+    public class profileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
