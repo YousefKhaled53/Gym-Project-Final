f184222ed46e6b5d5e10eb9cf0338cbfbed0555a
abdulrahman aboshareb <117599976+Abdoaboshareb@users.noreply.github.com>
2023-05-11 00:48:35 +0300

    hjkl

--- Gym Project/Pages/homepage_3.cshtml.cs
+++ Gym Project/Pages/homepage_3.cshtml.cs
@@ -2,6 +2,8 @@ using Gym_Project.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Data;
+using System.Net.Mail;
+using System.Net;
 
 namespace Gym_Project.Pages
 {
@@ -23,6 +25,27 @@ namespace Gym_Project.Pages
         public void OnGet()
         {
             dt = db.return_users_Feedback();
+        }
+        public IActionResult OnPostSubmitrequest()
+        {
+            string from = "abdoaboshareb8@gmail.com";
+            string from_pass = "bafpjaukobocrpnq";
+            string to = "s-abdel-rahman.ahmed@zewailcity.edu.eg";
+            string password = db.getpassword(Request.Form["username"]);
+            MailMessage msg = new MailMessage();
+            msg.From = new MailAddress(from); // Specify the sender's email address
+            msg.Subject =  Request.Form["maintenance-type"] +"";
+            msg.To.Add(new MailAddress(to));
+            msg.Body = "<html><body> "+ Request.Form["comment"] + "</body></html>";
+            msg.IsBodyHtml = true;
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(from, from_pass),
+                EnableSsl = true,
+            };
+            smtpClient.Send(msg);
+            return RedirectToPage("/homepage_3", new { username_coming_from_login = Request.Form["un"] });
         }
     }
 }
