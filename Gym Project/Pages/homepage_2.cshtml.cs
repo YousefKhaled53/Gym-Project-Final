b048dcdd4d77aa760d9fc21457241bf5163ee13c
Yousef Khaled <117599930+YousefKhaled53@users.noreply.github.com>
2023-05-11 16:47:18 +0300

    charts

--- Gym Project/Pages/homepage_2.cshtml.cs
+++ Gym Project/Pages/homepage_2.cshtml.cs
@@ -1,15 +1,20 @@
+using ChartExample.Models.Chart;
 using Gym_Project.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Newtonsoft.Json;
+using System.Reflection.Emit;
 
 namespace Gym_Project.Pages
 {
     public class homepage_2Model : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public ChartJs Chart { get; set; }
+        public string ChartJson { get; set; }
 
-     
+
         public body_info body_info;
         private readonly GYM_DB db;
         string subscribtion { get; set; }
@@ -26,7 +31,120 @@ namespace Gym_Project.Pages
         public void OnGet()
         {
             body_info= new body_info(username_coming_from_login,db.getweight(username_coming_from_login),db.getheight(username_coming_from_login),db.getmusclemass(username_coming_from_login),db.getfatpercentage(username_coming_from_login));
-        }
+            var chartData = @"
+
+{
+type: 'line',
+responsive: true,
+data:
+{
+labels: ['January', 'February', 'March', 'April','May','June','July','August','September','October','November','December'],
+datasets: [
+{
+label: 'Weight',
+data: [60, 65, 63, 65, 62, 63,60, 65, 63, 65, 62, 63],
+backgroundColor: [
+'rgba(255, 99, 132, 0.2)',
+'rgba(54, 162, 235, 0.2)',
+'rgba(255, 206, 86, 0.2)',
+'rgba(75, 192, 192, 0.2)',
+'rgba(153, 102, 255, 0.2)',
+'rgba(255, 159, 64, 0.2)'
+],
+borderColor: [
+'rgba(255, 99, 132, 1)',
+'rgba(54, 162, 235, 1)',
+'rgba(255, 206, 86, 1)',
+'rgba(75, 192, 192, 1)',
+'rgba(153, 102, 255, 1)',
+'rgba(255, 159, 64, 1)'
+],
+borderWidth: 1
+},
+{
+label: 'Height',
+data: [170, 171, 171, 172, 172, 172, 172, 172, 172, 173, 173, 173],
+backgroundColor: [
+'rgba(255, 99, 132, 0.2)',
+'rgba(54, 162, 235, 0.2)',
+'rgba(255, 206, 86, 0.2)',
+'rgba(75, 192, 192, 0.2)',
+'rgba(153, 102, 255, 0.2)',
+'rgba(255, 159, 64, 0.2)'
+],
+borderColor: [
+'rgba(255, 99, 132, 1)',
+'rgba(54, 162, 235, 1)',
+'rgba(255, 206, 86, 1)',
+'rgba(75, 192, 192, 1)',
+'rgba(153, 102, 255, 1)',
+'rgba(255, 159, 64, 1)'
+],
+borderWidth: 1
+},
+{
+label: 'Fat Percentage',
+data: [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9],
+backgroundColor: [
+'rgba(255, 99, 132, 0.2)',
+'rgba(54, 162, 235, 0.2)',
+'rgba(255, 206, 86, 0.2)',
+'rgba(75, 192, 192, 0.2)',
+'rgba(153, 102, 255, 0.2)',
+'rgba(255, 159, 64, 0.2)'
+],
+borderColor: [
+'rgba(255, 99, 132, 1)',
+'rgba(54, 162, 235, 1)',
+'rgba(255, 206, 86, 1)',
+'rgba(75, 192, 192, 1)',
+'rgba(153, 102, 255, 1)',
+'rgba(255, 159, 64, 1)'
+],
+borderWidth: 1
+},
+{
+label: 'Muscle Mass',
+data: [50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72],
+backgroundColor: [
+'rgba(255, 99, 132, 0.2)',
+'rgba(54, 162, 235, 0.2)',
+'rgba(255, 206, 86, 0.2)',
+'rgba(75, 192, 192, 0.2)',
+'rgba(153, 102, 255, 0.2)',
+'rgba(255, 159, 64, 0.2)'
+],
+borderColor: [
+'rgba(255, 99, 132, 1)',
+'rgba(54, 162, 235, 1)',
+'rgba(255, 206, 86, 1)',
+'rgba(75, 192, 192, 1)',
+'rgba(153, 102, 255, 1)',
+'rgba(255, 159, 64, 1)'
+],
+borderWidth: 1
+}
+]
+},
+options:
+{
+scales:
+{
+y: [{
+ticks:
+{
+beginAtZero: true
+}
+}]
+}
+}
+}"; //end of chartdata
+            Chart = JsonConvert.DeserializeObject<ChartJs>(chartData);
+            ChartJson = JsonConvert.SerializeObject(Chart, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+            });
+        } //end of OnGet()
         public IActionResult OnPostM1()
         {
 
